7b40821092613140999d408c277b5f9535ccc306
Abhishek Yadav <50040484+abhishekyadav-aps@users.noreply.github.com>
2020-09-29 07:49:54 +0530

    merge branch

--- app/src/main/java/com/adoptvillage/bridge/fragment/HomeFragment.kt
+++ app/src/main/java/com/adoptvillage/bridge/fragment/HomeFragment.kt
@@ -1,20 +1,47 @@
-package com.adoptvillage.bridge.fragment
+package com.adoptvillage.bridge.Fragment
 
+import android.app.ActionBar
 import android.os.Bundle
+import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.transition.TransitionInflater
+import androidx.viewpager.widget.ViewPager
+import com.adoptvillage.bridge.Models.CardAdapter
+import com.adoptvillage.bridge.Models.CardModel
 import com.adoptvillage.bridge.R
+import kotlinx.android.synthetic.main.fragment_home.*
 
 
 class HomeFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        val inflater = TransitionInflater.from(requireContext())
-        enterTransition = inflater.inflateTransition(R.transition.explode)
+
+    private lateinit var CardModelList: ArrayList<CardModel>
+    private lateinit var cardAdapter: CardAdapter
+
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        loadCards()
+    }
+
+    private fun loadCards()
+    {
+        CardModelList = ArrayList()
+
+        CardModelList.add(CardModel("101","Abhi","Vatsal","300"))
+        CardModelList.add(CardModel("102","Ankit","Abhishek","500"))
+        CardModelList.add(CardModel("103","Raju","Rasmeet","1000"))
+        CardModelList.add(CardModel("104","Humraz","John Doe","400"))
+
+        cardAdapter = activity?.let { CardAdapter(it,CardModelList) }!!
+
+        slideView.adapter = cardAdapter
+
+        slideView.setPadding(20,10,20,10)
+
     }
 
     override fun onCreateView(
@@ -27,4 +54,3 @@ class HomeFragment : Fragment() {
 
 
 }
-
