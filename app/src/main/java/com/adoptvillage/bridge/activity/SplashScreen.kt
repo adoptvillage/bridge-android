46bb15c896cac9f3e89a8ea448a25bf7e9dc358a
Abhishek Yadav <50040484+abhishekyadav-aps@users.noreply.github.com>
2020-10-16 17:23:17 +0530

    splashScreen bug fixed
    idtoken call in splash screen
    bug fix

--- app/src/main/java/com/adoptvillage/bridge/activity/SplashScreen.kt
+++ app/src/main/java/com/adoptvillage/bridge/activity/SplashScreen.kt
@@ -4,8 +4,11 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.adoptvillage.bridge.R
+import com.adoptvillage.bridge.service.RetrofitClient
+import com.google.firebase.auth.FirebaseAuth
 
 /* Splash Screen
 Wait for 2 sec
@@ -13,7 +16,11 @@ Check loggedIn or not
  */
 
 class SplashScreen : AppCompatActivity() {
+    var idTokenn=""
+    lateinit var mAuth: FirebaseAuth
     lateinit var countdownTimer:CountDownTimer
+    var isSplashScreenDone=false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
@@ -22,7 +29,7 @@ class SplashScreen : AppCompatActivity() {
             getString(R.string.parent_package_name),
             Context.MODE_PRIVATE
         )
-
+        mAuth= FirebaseAuth.getInstance()
         //Logged in or not
         val isLoggedIn=prefs.getBoolean(getString(R.string.is_Logged_In), false)
 
@@ -35,25 +42,48 @@ class SplashScreen : AppCompatActivity() {
         }
         countdownTimer.start()
     }
+    private fun getIDToken() {
+        idTokenn = ""
+        mAuth.currentUser!!.getIdToken(true).addOnCompleteListener {
+            if (it.isSuccessful) {
+                idTokenn = it.result!!.token!!
+                RetrofitClient.instance.idToken = idTokenn
+                Log.i("Testing",idTokenn)
+            }
+        }
+    }
 
     //calling next activity
     private fun callIntent(loggedIn: Boolean) {
-        if(loggedIn){
+        if (!isSplashScreenDone) {
+            if (loggedIn) {
+                getIDToken()
+                goToDashboard()
+            } else {
+                goToAuth()
+            }
+        }
+    }
 
-            val intent=Intent(this, DashboardActivity::class.java)
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
-            startActivity(intent)
-        }
-        else{
-            val intent=Intent(this, MainActivity::class.java)
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
-            startActivity(intent)
-        }
+    private fun goToAuth() {
+        isSplashScreenDone=true
+        val intent=Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        startActivity(intent)
+    }
+
+    private fun goToDashboard() {
+        isSplashScreenDone=true
+        val intent=Intent(this, DashboardActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        startActivity(intent)
     }
 
     override fun onResume() {
         super.onResume()
-        countdownTimer.start()
+        if (!isSplashScreenDone) {
+            countdownTimer.start()
+        }
     }
 
 }
