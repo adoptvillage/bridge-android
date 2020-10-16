4fddbbb266fae1af656a3214725eeda81198aa48
AnkitCode99 <9999385728ankit@gmail.com>
2020-10-16 10:06:56 +0530

    Added next button code
    Fragment Change code for application detail-1

--- app/src/main/java/com/adoptvillage/bridge/fragment/applicationListFragment/ApplicationDetail_1.kt
+++ app/src/main/java/com/adoptvillage/bridge/fragment/applicationListFragment/ApplicationDetail_1.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.adoptvillage.bridge.R
+import com.adoptvillage.bridge.fragment.applicationFormFragment.ApplicationFormInstituteDetails
+import kotlinx.android.synthetic.main.fragment_application_detail_1.*
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
@@ -34,7 +36,15 @@ class ApplicationDetail_1 : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        btntvNextSetOnClickListener()
+    }
 
+    private fun btntvNextSetOnClickListener()
+    {
+        tvNext.setOnClickListener {
+            activity?.supportFragmentManager?.beginTransaction()
+                ?.replace(R.id.fl_wrapper_applications, ApplicationDetail_2())?.commit()
+        }
     }
 
 }
